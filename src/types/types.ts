5f3ddad37af70f2a5196c5c9e44e6f8ea87b342a
Muhammad-amin7 <abduhalilovshohruh681@gmail.com>
2025-10-24 16:04:26 +0500

    Create v1

--- src/types/types.ts
+++
@@ -1,3 +0,0 @@
-export type stageType = "success" | "in-progress" | "to-do"
-export type itemType = {title: string , id: string , done: boolean}
-export type TaskType = { id:string , title: string ,expire: string , status: stageType , items: itemType[] , group_id: number, created: Date }
